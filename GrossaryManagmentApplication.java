3fc621bbd3835079fffe2288a968000054924621
khushavnt-cognitiona <136231013+khushavnt-cognitiona@users.noreply.github.com>
2023-07-21 10:08:52 +0530

    Add files via upload

---
+++ GrossaryManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrossaryManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrossaryManagmentApplication.class, args);
+	}
+
+}
